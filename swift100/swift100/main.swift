56c966da5525e572cf14d54a946a550261edc766
furkan <44947688+f2rkan@users.noreply.github.com>
2022-12-05 00:08:45 +0300

    fundamentals and syntax
    --not hello world

---
+++ swift100/swift100/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  swift100
+//
+//  Created by Ömer Furkan SAĞIR on 2.12.2022.
+//
+
+import Foundation
+
+var degisken1 : Int = 33
+print(degisken1)
+
+var degisken2 : Double = 57
+print(degisken2)
+
+var degisken3 : Bool
+if degisken2 < 57 {
+    degisken3 = false
+}
+else {
+    degisken3 = true
+}
+print(degisken3)
+
+var degisken4 = true
+print(degisken4)
+
+var degisken5 : String = "deneme"
+print(degisken5)
+
